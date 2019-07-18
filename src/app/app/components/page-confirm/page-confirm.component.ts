2d78c6e8ada8ba81742097eeeba07a6f7bd16dfa
sebastiangolian <sebastiangolian@gmail.com>
2019-07-18 09:48:49 +0200

    add checkbox rules

--- src/app/app/components/page-confirm/page-confirm.component.ts
+++ src/app/app/components/page-confirm/page-confirm.component.ts
@@ -11,20 +11,27 @@ export class PageConfirmComponent implements OnInit {
 
   private resignationUrl: string = "https://www.google.com"
   private acceptUrl: string = "/home"
+
+  public isEnabled:boolean = false
   public config:any = {show: true, ignoreBackdropClick: true, keyboard: false}
 
   constructor(private router: Router, private rulesService: RulesService) {}
    
   ngOnInit() {}
 
-  accept()
-  {
+  accept() {
+    if(this.isEnabled == true)
+      this.isEnabled = false
+    else
+      this.isEnabled = true
+  }
+
+  confirm() {
     this.rulesService.setRules()
     this.router.navigate([this.acceptUrl])
   }
 
-  reject()
-  {
+  reject() {
     window.open(this.resignationUrl, '_self');
   }
 }
